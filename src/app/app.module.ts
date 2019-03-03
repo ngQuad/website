5381a5a321a454ef5ff236cd55a944ecd812088f
Miroslav Maksimovic <miki.maksimovic19995@gmail.com>
2019-03-03 14:05:24 +0100

    Home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { LocationItemComponent } from './components/map/location-item/location-i
 import { AboutComponent } from './components/about/about.component';
 import { ServicesComponent } from './components/services/services.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HeaderComponent } from './components/header/header.component';
     LocationItemComponent,
     AboutComponent,
     ServicesComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
