449a5b9f0b154590769f595d0e53733a8418b34c
Miroslav Maksimovic <miki.maksimovic19995@gmail.com>
2019-03-09 13:15:48 +0100

    Wake up Heroku Server!

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -8,7 +8,7 @@ import { IEmail } from '../interfaces/email.interface';
 export class AppService {
 
   private emailUrl = config.apiEmailEndpoint + '/send-email';  // URL to ngQuad mail web api
-
+  private wakeUpUrl = config.apiEmailEndpoint + '/wake-up';  // URL to ngQuad wake up Heroku Server web api
 
   constructor(private httpService: HttpService) {
   }
@@ -16,5 +16,11 @@ export class AppService {
   sendEmail(email: IEmail) {
 
     return this.httpService.post(`${this.emailUrl}`, JSON.stringify(email));
+  }
+
+  wakeUpServer() {
+
+    return this.httpService.get(`${this.wakeUpUrl}`).subscribe((res) => {
+    });
   }
 }
